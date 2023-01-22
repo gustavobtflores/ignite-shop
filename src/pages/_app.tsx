b823616f64673bb169be0ab72ab0899014ff91f6
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-01-22 20:18:35 -0300

    deletes unnecessary files and setups project font

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+
+import { Roboto_Flex } from "@next/font/google";
+
+const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={robotoFlex.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
