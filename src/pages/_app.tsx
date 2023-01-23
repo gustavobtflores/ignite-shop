66ebb4a6d336064fcf4d9f8dc37f02353bccc71c
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-01-23 18:14:36 -0300

    basic style structure and adds keen-slider to handle carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,32 @@
 import type { AppProps } from "next/app";
 
 import { Roboto_Flex } from "@next/font/google";
+import { globalStyles } from "../styles/global";
+
+import logoImg from "../assets/logo.svg";
+import Image from "next/image";
+import { Container, Header } from "../styles/pages/app";
 
 const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
 
+globalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={robotoFlex.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${robotoFlex.style.fontFamily};
+          }
+        `}
+      </style>
+      <Container>
+        <Header>
+          <Image src={logoImg} alt="" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,47 @@
-import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
+import Image from "next/image";
 
-const Button = styled("button", {
-  backgroundColor: "$rocketseat",
-  border: "none",
-  padding: "8px",
-  borderRadius: 4,
-  cursor: "pointer",
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import camisa1 from "../assets/camisa1.png";
+import camisa2 from "../assets/camisa2.png";
+import camisa3 from "../assets/camisa3.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  });
+
   return (
-    <>
-      <Button>Hello world</Button>
-    </>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camisa1} width={520} height={480} alt="" quality={100} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} width={520} height={480} alt="" quality={100} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={480} alt="" quality={100} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
