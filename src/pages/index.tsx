3441ffd57efb79dd715724fdcb27e3bb919f58f3
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-01-23 15:27:51 -0300

    basic stitches configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  border: "none",
+  padding: "8px",
+  borderRadius: 4,
+  cursor: "pointer",
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
 export default function Home() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Button>Hello world</Button>
     </>
   );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,19 @@
+import {createStitches} from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  globalCss, 
+  keyframes,
+  theme,
+  createTheme, 
+  getCssText, 
+  css
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: "#8257e6"
+    }
+  }
+})
+
